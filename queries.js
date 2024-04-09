d63a47a91b8a0b8b2985d457441404010a3aadc9
Stewpra <wilbstewart7@gmail.com>
2024-04-08 22:11:22 -0400

    get all departments

--- queries.js
+++ queries.js
@@ -1,1 +1,22 @@
 const pool = require('./server.js');
+
+class queries {
+  // function to execute queries
+  static async executeQuery(sql, params = []) {
+    const client = await pool.connect();
+    try {
+      const res = await client.query(sql, params);
+      return res.rows;
+    } catch (err) {
+      console.error('Error executing query:', err);
+      return 8;
+    } finally {
+      client.release();
+    }
+  }
+
+  // Get all departments
+  static async getAllDepartments() {
+    return this.executeQuery(`SELECT * FROM department;`);
+  }
+}
