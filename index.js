60a52434a2a3768cfc0b6bbe53d0e3b0dcc4b9c6
Stewpra <wilbstewart7@gmail.com>
2024-04-08 21:41:10 -0400

    server queries and index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 
+const queries = require('./queries');
+
 const questions = [
   {
     type: 'list',
@@ -13,20 +15,72 @@ const questions = [
       'Add role',
       'View all departments', // GET
       'Add a department',
-      'Update employee role',
       'Quit',
     ],
   },
 
   // Add employee
+  {
+    type: 'input',
+    name: 'firstName',
+    when: (answers) => answers.welcome === 'Add an employee',
+    message: "What is the employee's first name?",
+  },
+  {
+    type: 'input',
+    name: 'lastName',
+    when: (answers) => answers.welcome === 'Add an employee',
+    message: "What is the employee's last name?",
+  },
+  {
+    type: 'input',
+    name: 'roleTitle',
+    when: (answers) => answers.welcome === 'Add an employee',
+    message: "Enter the title of the employee's role:",
+  },
+  {
+    //get list of all managers and select one. also have option for no manager
+  },
 
-  // Update employee role
+  //Update employee role
+  //get list of all employees and select one
+  //get list of roles and select one
 
-  // Add role
+  //Add role
+  {
+    type: 'input',
+    name: 'newRoleTitle',
+    when: (answers) => answers.welcome === 'Add a role',
+    message: 'What is the title of the new role?',
+  },
+  {
+    type: 'input',
+    name: 'newRoleSalary',
+    when: (answers) => answers.welcome === 'Add a role',
+    message: 'What is the salary for this role?',
+    validate: (value) => {
+      const valid = !isNaN(parseFloat(value));
+      return valid || 'Please enter a number for the salary';
+    },
+  },
+  {
+    // get list of all departments and select one
+  },
 
   //Add a department
-
-  //update employee role
+  {
+    type: 'input',
+    name: 'adddepartment',
+    when: (answers) => answers.welcome === 'Add a department',
+    message: 'What is the name of the department?',
+  },
 
   //quit
+  {
+    type: 'confirm',
+    name: 'confirmQuit',
+    when: (answers) => answers.welcome === 'Quit',
+    message: 'Are you sure you want to quit?',
+    default: false,
+  },
 ];
