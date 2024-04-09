235c7b3de050f5ced838e14ab798a65c03be8224
Stewpra <wilbstewart7@gmail.com>
2024-04-08 22:28:46 -0400

    added get for emplyees and roles

--- queries.js
+++ queries.js
@@ -19,4 +19,37 @@ class queries {
   static async getAllDepartments() {
     return this.executeQuery(`SELECT * FROM department;`);
   }
+
+  // Get all roles
+  static async getAllRoles() {
+    return this.executeQuery(
+      `SELECT role.id, role.title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id;`
+    );
+  }
+
+  // Get all employees
+  static async getAllEmployees() {
+    return this.executeQuery(`
+      SELECT
+        employee.id,
+        employee.first_name,
+        employee.last_name,
+        role.title,
+        role.salary AS salary,
+        department.name AS department,
+        CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+      FROM
+        employee
+        JOIN role ON employee.role_id = role.id
+        JOIN department ON role.department_id = department.id
+        LEFT JOIN employee as manager ON employee.manager_id = manager.id;`);
+  }
+
+  // Add department
+
+  // Add role
+
+  // Add employee
+
+  // Update employee role
 }
